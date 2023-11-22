b6e37955d5246d28899d6ec80b8c6a513b435470
venkateshwar <kamlekar.venkatesh@gmail.com>
2023-11-22 15:35:36 +0530

    fix: make the value 0 if null. To avoid deprecation warning

--- admin/index.php
+++ admin/index.php
@@ -399,7 +399,7 @@ if (isset($_GET['search-query'])) {
                             if ($result->num_rows > 0) {
                                 $row = $result->fetch_assoc();
                                 $totalPaidAmount = $row["total_paidamount"];
-                                echo "₹" . number_format($totalPaidAmount, 2);
+                                echo "₹" . number_format($totalPaidAmount || 0, 2);
                             } else {
                                 echo "No payments recorded for this month.";
                             }
